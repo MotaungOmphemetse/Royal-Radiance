0ae477f9e16d3be8224840d9c87e34be89c14198
Nosipho19 <hanyanenosipho@gmail.com>
2023-11-30 14:29:54 +0200

    commit

--- royal/src/app/get-single-user.service.ts
+++ royal/src/app/get-single-user.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { catchError, tap, throwIfEmpty } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { UserData } from './user/user';
+import { User } from './user/user.component';
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class Userservice {
-  
+
   private productURL = 'https://fakestoreapi.com/users';
   constructor(private http: HttpClient) {}
 
@@ -19,10 +21,12 @@ export class Userservice {
   // }
 
 
-  getUsers(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.productURL).pipe(
-      tap((_) => this.log('fetched products')), // Updated log message
-      catchError(this.handleError<Product[]>('getProducts', []))
+
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.productURL).pipe(
+      tap((_) => this.log('fetched users')), // Updated log message
+      
     );
   }
 
